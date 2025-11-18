9bf602d9c60457894b9ea0b11692e487eab87924
edgarsegundo <edgar.segundo@gmail.com>
2025-11-18 15:18:51 -0300

    Checkpoint at 18-Nov-25 15:18:51

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # egar
+
+## Development
+
+npm run watch:css
+
+## Production
+
+npm run build:css
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./public/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
